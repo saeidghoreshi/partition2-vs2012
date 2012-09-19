2ef392a67cc1e6beccebf78189e40db9fe124400
ryan goreshi <ryan.goreshi@yahoo.com>
2012-09-19 16:42:23 -0700

    commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,45 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            testAsync ins = new testAsync();
+            ins.runAsync();
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
         }
     }
+
     public class testAsync 
     {
-        public async void run() { }
+        public void runSync() 
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://www.google.com");
+            request.Method = "Head";
+            var response = (HttpWebResponse)request.GetResponse();
+            string headersText = formatOutput(response.Headers);
+            Console.WriteLine(headersText);
+
+        }
+        public void runAsync()
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://www.google.com");
+            request.Method = "Head";
+            request.BeginGetResponse(
+                asyncResult => 
+                {
+                    var response = (HttpWebResponse)request.EndGetResponse(asyncResult);
+                    string headersText = formatOutput(response.Headers);
+                    Console.WriteLine(headersText);
+                },null
+                );
+        }
+        public string formatOutput(WebHeaderCollection input) 
+        {
+            
+            var query = input.Keys.Cast<string>().Select(x => x+" >>>> "+input[x]).ToList();
+            return string.Join(Environment.NewLine, query);
+        
+        }
     }
 }
